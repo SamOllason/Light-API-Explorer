ad4e6a167052895a9ed953d06ae588d244dbf8db
Sam Ollason <sam.ollason@howdeninsurance.co.uk>
2026-01-08 20:21:49 +0000

    Fix basePath for GitHub Pages deployment

--- apps/demo/next.config.js
+++ apps/demo/next.config.js
@@ -5,9 +5,9 @@ const nextConfig = {
   // For GitHub Pages static export
   output: 'export',
   
-  // Deploying to https://<username>.github.io/light-faux-sdk/
-  basePath: '/light-faux-sdk',
-  assetPrefix: '/light-faux-sdk/',
+  // Deploying to https://samollason.github.io/Light-demo-SDK-exploring/
+  basePath: '/Light-demo-SDK-exploring',
+  assetPrefix: '/Light-demo-SDK-exploring/',
   
   // Disable image optimization (not supported in static export)
   images: {
